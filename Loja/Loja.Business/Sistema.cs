ed76ab225b9f4c3304a0d6678b05241e317435b7
Eliezer Ap. F. da Silva <eliezerferreira1518@gmail.com>
2022-11-29 23:28:02 -0300

    Adicionar arquivos iniciais do projeto

---
+++ Loja/Loja.Business/Sistema.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Business
+{
+    public class Sistema
+    {
+        public static ISession Sessao { get { return Data.CacheDados.Sessao; } }
+    }
+}
---
+++ Loja/Loja.Data/UniqueClass/DAOComando.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace Loja.Data.UniqueClass
+{
+    public class DAOComando
+    {
+        public object Objeto { get; set; }
+        public TipoComandoSQL TipoComandoSQL { get; set; }
+        public DAOComando() { }
+        public DAOComando(object objeto, TipoComandoSQL tipoComandoSQL)
+        {
+            Objeto = objeto;
+            TipoComandoSQL = tipoComandoSQL;
+        }
+    }
+}
